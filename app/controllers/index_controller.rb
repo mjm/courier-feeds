4bfa0879d03680bb2fbd48f78ddb6ecc54009f79
Matt Moriarity <matt@mattmoriarity.com>
2018-07-17 18:35:11 -0500

    Add basic index page explaining the service

---
+++ app/controllers/index_controller.rb
@@ -0,0 +1,19 @@
+class IndexController < ApplicationController
+  get '/' do
+    markdown <<'HOME'
+# Courier Feeds
+
+This is a microservice that tracks feeds that users have set up in Courier.
+Feeds are tracked uniquely (there is a single record for each distinct feed URL).
+Two users can pull from the same feed, but we will only query it once.
+Courier Feeds currently only supports JSON Feed.
+
+## How to Use
+
+### GET `/feeds`
+
+Lists all of the feeds the the service is tracking.
+Returns a JSON array with information about all tracked feeds.
+HOME
+  end
+end
