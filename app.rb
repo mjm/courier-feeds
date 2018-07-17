c0182e96b8923bf8935ed00932eead88d86f5046
Matt Moriarity <matt@mattmoriarity.com>
2018-07-17 18:48:33 -0500

    Move index page into a template file

--- app.rb
+++ app.rb
@@ -5,10 +5,12 @@ class ApplicationController < Sinatra::Base
   def self.inherited(subclass)
     super
     App.use subclass
+    subclass.set :root, App.root
   end
 end
 
 class App < Sinatra::Base
   disable :show_exceptions
+  set :root, File.join(__dir__, 'app')
   require_app :controllers
 end
